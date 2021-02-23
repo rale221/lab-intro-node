1a811fc47cb0f567ad82c9814a954c17838eb7f5
Rastko <rastkodjurica@yahoo.com>
2021-02-23 15:37:24 -0500

    IterationOne

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 class SortedList {
-  constructor() {}
+  constructor() { }
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) { }
+  add(item) {
+    this.items.push(item)
+    this.length += this.items.length
+    this.items.sort((a, b) => a - b)
+  }
 
-  get(pos) {}
+
+  get(pos) { }
+  let something = this.items[pos]
+if (something) {
+  return something
+}
+else ([pos > this.items.length) {
+  throw new Error('OutOfBounds')
+}
+
 
   max() {}
 
